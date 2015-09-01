cdd475928c324bd422b2d16f9709638c0137ca15
elliot <e.mitchell968@gmail.com>
2015-09-01 09:14:17 +0100

    daily_transaction

---
+++ daily_transaction/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Daily Transaction Manager',
+    'version': '1.0',
+    'category': 'manager',
+    'description': "",
+
+    'author': 'Elliot Mitchell',
+    'depends': [],
+    'data': ['daily_transaction_view.xml'],
+    'demo': [],
+    'installable': True, 
+    'auto_install': False,
+}
---
+++ daily_transaction/daily_transaction.py
@@ -0,0 +1,18 @@
+from openrp.osv import fields, osv
+
+class daily_transaction(osv.osv):
+    _name = "daily.transaction"
+    _description = "Daily Transaction"
+    
+    _column = {
+            'subject': fields.char('Subject', size=128, required=True),
+            'date': fields.date('Date',required=True),
+            'note': fields.text('Notes'),
+            'amount': fields.float('Amount', required=True),
+            'type': fields.selection([
+                ('transport', 'Transport'),
+                ('household', 'Household'),
+                ('personal', 'Personal'),
+                ], 'Type', required=True),
+               
+               }
